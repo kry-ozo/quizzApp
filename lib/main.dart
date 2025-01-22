c72f3d9362e5aac9396c3f0907a7a592401090d9
Kryozo <dorentier124@gmail.com>
2025-01-22 14:42:59 +0100

    Added bottom navbar, not working for now

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:circle_nav_bar/circle_nav_bar.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,50 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
-        appBar: AppBar,
         body: Center(
           child: Text('Hello World!'),
         ),
+        bottomNavigationBar: CircleNavBar(
+        activeIndex: 0,
+        activeIcons: const [
+          Icon(Icons.person, color: Colors.deepPurple),
+          Icon(Icons.home, color: Colors.deepPurple),
+          Icon(Icons.favorite, color: Colors.deepPurple),
+        ],
+        inactiveIcons: const [
+          Text("My"),
+          Text("Home"),
+          Text("Like"),
+        ],
+        color: Colors.white,
+        circleColor: Colors.white,
+        height: 60,
+        circleWidth: 60,
+
+        // tabCurve: ,
+        
+        cornerRadius: const BorderRadius.only(
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+          bottomRight: Radius.circular(24),
+          bottomLeft: Radius.circular(24),
+        ),
+        shadowColor: Colors.deepPurple,
+        circleShadowColor: Colors.deepPurple,
+        elevation: 10,
+        gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [ Colors.blue, Colors.red ],
+        ),
+        circleGradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [ Colors.blue, Colors.red ],
+        ),
+      ),
       ),
     );
   }
