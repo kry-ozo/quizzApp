1911bcd74d669ee5c2e56ee78c303e07b680df60
kry-ozo <91698605+kry-ozo@users.noreply.github.com>
2025-01-28 21:17:40 +0100

    Added files for future pages

---
+++ lib/pages/quizz_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuizzPage extends StatelessWidget {
+  const QuizzPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  Center(
+      child: Text("QUIZZ PAGE"),
+    );
+  }
+}
