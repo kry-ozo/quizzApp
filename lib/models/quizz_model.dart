b5abf9f186c2c2d3e62ce08e480755c2e24f3e19
kry-ozo <91698605+kry-ozo@users.noreply.github.com>
2025-01-29 22:43:38 +0100

    Added factory for fetching data

--- lib/models/quizz_model.dart
+++ lib/models/quizz_model.dart
@@ -6,4 +6,19 @@ class QuizzModel {
   final String correctAnswer;
 
   QuizzModel({required this.type, required this.difficulty, required this.category, required this.question, required this.correctAnswer});
+
+  factory QuizzModel.fromJson(Map<String, dynamic> json){
+    return switch(json){
+      {
+        'type' : String type,
+        'difficulty' : String difficulty,
+        'category' : String category,
+        'question' : String question,
+        'correct_anwear' : String correctAnswer
+      }
+      => QuizzModel(type: type, difficulty: difficulty, category: category, question: question, correctAnswer: correctAnswer),
+      _=> throw const FormatException("Failed to load question"),
+    };
+  }
 }
+
