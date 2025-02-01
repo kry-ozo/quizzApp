db60686fd3c528fe23fbc7f4cd2a225ab7f2e711
kry-ozo <91698605+kry-ozo@users.noreply.github.com>
2025-02-01 23:18:12 +0100

    Changed way of getting data from api

--- lib/models/quizz_model.dart
+++ lib/models/quizz_model.dart
@@ -4,21 +4,19 @@ class QuizzModel {
   final String category;
   final String question;
   final String correctAnswer;
+  List<String> incorrectAnswers;
 
-  QuizzModel({required this.type, required this.difficulty, required this.category, required this.question, required this.correctAnswer});
+  QuizzModel({required this.type, required this.difficulty, required this.category, required this.question, required this.correctAnswer, required this.incorrectAnswers});
 
   factory QuizzModel.fromJson(Map<String, dynamic> json){
-    return switch(json){
-      {
-        'type' : String type,
-        'difficulty' : String difficulty,
-        'category' : String category,
-        'question' : String question,
-        'correct_anwear' : String correctAnswer
-      }
-      => QuizzModel(type: type, difficulty: difficulty, category: category, question: question, correctAnswer: correctAnswer),
-      _=> throw const FormatException("Failed to load question"),
-    };
+    return QuizzModel(
+      type: json['type'],
+      difficulty: json['difficulty'],
+      category: json['category'],
+      question: json['question'],
+      correctAnswer: json['correct_answer'],
+      incorrectAnswers: List<String>.from(json['incorrect_answers']),
+    );
   }
 }
 
