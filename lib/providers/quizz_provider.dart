88e98e34fae92568686510056e4959a96e772c1c
kry-ozo <91698605+kry-ozo@users.noreply.github.com>
2025-02-01 23:33:50 +0100

    Added function to fetch data, for now i will focus on frontend and picking some quizz rather than showing

--- lib/providers/quizz_provider.dart
+++ lib/providers/quizz_provider.dart
@@ -1,8 +1,19 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:quizz_app/models/quizz_model.dart';
 
 class QuizzProvider extends ChangeNotifier{
-  Future<http.Response> _getApiKey(){
-    return http.get(Uri.parse('https://opentdb.com/api_token.php?command=request'));
-  }
+  
+  Future<QuizzModel>fetchQuizzQuestion(String category, String diff) async {
+    final response = await http.get(Uri.parse("https://opentdb.com/api.php?amount=1&category=$category&difficulty=$diff"));
+    if(response.statusCode == 0){
+      return QuizzModel.fromJson(jsonDecode(response.body) as Map <String, dynamic>);
+    }else{
+      throw Exception("Failed to load question");
+    }
+  } 
+
+  
 }
