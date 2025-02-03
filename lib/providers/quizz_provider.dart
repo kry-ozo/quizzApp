168a4344ecab17f3bccffa4f2aeaad79e2b0d393
kry-ozo <91698605+kry-ozo@users.noreply.github.com>
2025-02-03 21:05:29 +0100

    Changed the way of storing data from fetching, now its in array

--- lib/providers/quizz_provider.dart
+++ lib/providers/quizz_provider.dart
@@ -6,10 +6,13 @@ import 'package:quizz_app/models/quizz_model.dart';
 
 class QuizzProvider extends ChangeNotifier{
   
-  Future<QuizzModel>fetchQuizzQuestion(String category, String diff) async {
-    final response = await http.get(Uri.parse("https://opentdb.com/api.php?amount=1&category=$category&difficulty=$diff"));
+  Future<List<QuizzModel>>fetchQuizzQuestions(String category, String diff, String amount) async {
+    final response = await http.get(Uri.parse("https://opentdb.com/api.php?amount=$amount&category=$category&difficulty=$diff"));
     if(response.statusCode == 0){
-      return QuizzModel.fromJson(jsonDecode(response.body) as Map <String, dynamic>);
+      var jsonData = jsonDecode(response.body);
+      return (jsonData['results'] as List).map(
+        (item)=> QuizzModel.fromJson(item)
+      ).toList();
     }else{
       throw Exception("Failed to load question");
     }
