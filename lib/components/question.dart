d0927714938c9b0a59b1c8abecaaa84ac416783e
kry-ozo <91698605+kry-ozo@users.noreply.github.com>
2025-02-07 23:35:03 +0100

    Added full style for question

--- lib/components/question.dart
+++ lib/components/question.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
@@ -8,14 +10,44 @@ class Question extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Color.fromARGB(255, 255, 250, 255)
+      ),
+      height: 350,
+      width: MediaQuery.of(context).size.width,
       child: Column(
         children: [
-          Text(question),
+          Container(
+            child: Text(question, style: TextStyle(color: Color.fromARGB(255, 255, 250, 255), fontSize: 24), textAlign: TextAlign.center),
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.symmetric(vertical: 10),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 0, 53, 84),
+              borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+            ),
+            ),
           Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: answears.map((answear)=> Container(
-              child: Text(answear)
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 115, 166, 173),
+                
+              ),
+              padding: EdgeInsets.symmetric(vertical: 6),
+              child: Text(answear, textAlign: TextAlign.center, style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 255, 250, 255)),)
             )).toList(),
+          ),
+          Container(
+            height: 30,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 0, 53, 84),
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))
+            ),
           )
         ],
       ),
--- lib/models/quizz_model.dart
+++ lib/models/quizz_model.dart
@@ -11,8 +11,6 @@ class QuizzModel {
   QuizzModel({required this.type, required this.difficulty, required this.category, required this.question, required this.correctAnswer, required this.incorrectAnswers});
 
   factory QuizzModel.fromJson(Map<String, dynamic> json){
-    
-
     List<int> questionBytes = base64.decode(json['question']);
     String finalQuestion = utf8.decode(questionBytes);
 
