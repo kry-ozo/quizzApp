779eb298145bf4a570642e57fed8236dfa5efb2a
kry-ozo <91698605+kry-ozo@users.noreply.github.com>
2025-02-10 21:18:00 +0100

    I write a new func to fetch data, is more like the old version of it and for now its only fetch a hardcoded data

--- lib/providers/quizz_provider.dart
+++ lib/providers/quizz_provider.dart
@@ -6,8 +6,22 @@ import 'package:quizz_app/models/quizz_model.dart';
 
 class QuizzProvider extends ChangeNotifier{
     
-//TODO: write function to connect to api and gather a list of QuizzModel questions
-  
 
-  
+  Future<List<QuizzModel>> fetchQuizzList() async {
+    final response = await http.get(Uri.parse("https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple&encode=base64"));
+
+    try{
+      if(response.statusCode == 200){
+        var jsonData = jsonDecode(response.body);
+        List<QuizzModel> questions = (jsonData['results'] as List).map(
+          (item) => QuizzModel.fromJson(item)
+        ).toList();
+        return questions;
+      }else{
+        throw Exception("Failed to load questions");
+      }
+    }catch(e){
+      throw Exception(e.toString());
+    }
+  }
 }
