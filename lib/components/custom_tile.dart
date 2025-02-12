110a22ebf74d09cb5b73ea847cee3a3beff3179e
kry-ozo <91698605+kry-ozo@users.noreply.github.com>
2025-02-12 21:01:36 +0100

    Added functionality and design to quizz page

--- lib/components/custom_tile.dart
+++ lib/components/custom_tile.dart
@@ -9,14 +9,15 @@ class CustomTile extends StatelessWidget {
     return  Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: Color.fromARGB(255, 115, 166, 173),
+        color: const Color.fromARGB(255, 115, 166, 173),
       ),
       margin: const EdgeInsets.symmetric(horizontal: 16),
       width: 300,
+      height: 200,
       child:  Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(title, style: TextStyle(fontSize: 28, color: Color.fromARGB(255, 255, 250, 255), fontWeight: FontWeight.bold))
+          Text(title, style: const TextStyle(fontSize: 28, color: const Color.fromARGB(255, 255, 250, 255), fontWeight: FontWeight.bold))
         ],
       ),
     );
--- lib/pages/quizz_page.dart
+++ lib/pages/quizz_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/components/custom_tile.dart';
+import 'package:quizz_app/pages/quizz/quizz_settings.dart';
+import 'package:quizz_app/placeholders/categories.dart';
 
 class QuizzPage extends StatelessWidget {
   const QuizzPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const  Center(
-      child: Text("QUIZZ PAGE", style: TextStyle(color: Colors.black)),
+    return  Column(
+      children: [
+        const Padding(
+          padding: const EdgeInsets.only(top: 70, bottom: 20),
+          child: const Text("All Quizzes", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 0, 53, 84)),),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: categories.length,
+            itemBuilder: (context, index){
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: GestureDetector(
+                child: CustomTile(title: categories[index].catageoryName,),
+                onTap: (){
+                  Navigator.push(context,
+                  MaterialPageRoute(builder: (context)=> QuizzSettings(category: categories[index])));
+                },
+                ),
+              );
+          }),
+        ),
+      ],
     );
   }
 }
