6118039cdfd48025bd98185b01f86cb91d57f75b
kry-ozo <91698605+kry-ozo@users.noreply.github.com>
2025-03-05 20:41:40 +0100

    Added styling for account page

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -5,8 +5,43 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("ACCOUNT PAGE", style: TextStyle(color: Colors.black),),
+    return Column(
+      children:  [
+        SizedBox(height: 50,),
+        SizedBox(
+          
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                child: Text("Username", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 0, 53, 84)),),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 30,),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: Color.fromARGB(255, 255, 250, 255),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(255, 0, 53, 84),
+                blurRadius: 7,
+                spreadRadius: 2
+              )
+            ]
+          ),
+          child: SizedBox(
+            height: 80,
+            child: Icon(Icons.person, size: 70,color:Color.fromARGB(255, 0, 53, 84) ,),
+          ),
+        ),
+        SizedBox(height: 50,),
+        SizedBox(child: Text("Your last Quiz Scores", style: TextStyle(color:Color.fromARGB(255, 0, 53, 84), fontSize: 24),),),
+      ],
     );
   }
 }
